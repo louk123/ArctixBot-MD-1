14ac3d8098774aa0a1d627d86a7746dc0a0b24a9
Bomiro <105790086+Bomiro@users.noreply.github.com>
2022-07-05 00:43:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
+if(msg.key.rmoteJid.endsWith(m.chat)){
+
+sock.ban(user)
+
+}
+
 say('𝐀𝐫𝐜𝐭𝐢𝐱 × 𝐀𝐫𝐜𝐞𝐮𝐬 𝐢𝐧𝐜.\nsibssssssss', {
   font: 'chrome',
   align: 'center',
