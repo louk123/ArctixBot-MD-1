b31f010bbbb61a580a77653880d565beee202e78
Bomiro <105790086+Bomiro@users.noreply.github.com>
2022-07-05 11:39:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,8 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-if(msg.key.rmoteJid.endsWith(m.chat)){
 
-sock.ban(user)
 
-}
 
 say('𝐀𝐫𝐜𝐭𝐢𝐱 × 𝐀𝐫𝐜𝐞𝐮𝐬 𝐢𝐧𝐜.\nsibssssssss', {
   font: 'chrome',
